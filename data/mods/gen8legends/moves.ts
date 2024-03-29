a28cdccd4b8a20a95e59a850bac054338b175274
André Bastos Dias <andrebastosdias@gmail.com>
2024-03-29 22:48:30 +0000

    Make all Hidden Power types available

--- data/mods/gen8legends/moves.ts
+++ data/mods/gen8legends/moves.ts
@@ -263,6 +263,70 @@ export const Moves: {[k: string]: ModdedMoveData} = {
         inherit: true,
         isNonstandard: null,
     },
+    hiddenpowerbug: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerdark: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerdragon: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerelectric: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerfighting: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerfire: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerflying: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerghost: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowergrass: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerground: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerice: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerpoison: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerpsychic: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerrock: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowersteel: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    hiddenpowerwater: {
+        inherit: true,
+        isNonstandard: null,
+    },
     highhorsepower: {
         inherit: true,
         isNonstandard: null,
