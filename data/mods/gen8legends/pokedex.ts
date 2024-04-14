d85ea6bc193f1b402e349a0da2f089fcd1197187
André Bastos Dias <andrebastosdias@gmail.com>
2024-04-14 01:56:56 +0100

    Correct evolution methods

--- data/mods/gen8legends/pokedex.ts
+++ data/mods/gen8legends/pokedex.ts
@@ -1,4 +1,96 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	alakazam: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Linking Cord",
+	},
+	machamp: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Linking Cord",
+	},
+	golem: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Linking Cord",
+	},
+	gengar: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Linking Cord",
+	},
+	chansey: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Oval Stone",
+		evoCondition: "during the day",
+	},
+	quilava: {
+		inherit: true,
+		evos: ["Typhlosion-Hisui"],
+	},
+	steelix: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Metal Coat",
+	},
+	scizor: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Metal Coat",
+	},
+	weavile: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Razor Claw",
+		evoCondition: "at night",
+	},
+	magnezone: {
+		inherit: true,
+		evoType: "other",
+		evoCondition: "Thunder Stone or near a special magnetic field",
+	},
+	rhyperior: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Protector",
+	},
+	electivire: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Electirizer",
+	},
+	magmortar: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Magmarizer",
+	},
+	leafeon: {
+		inherit: true,
+		evoType: "other",
+		evoCondition: "Leaf Stone or near a Moss Rock",
+	},
+	glaceon: {
+		inherit: true,
+		evoType: "other",
+		evoCondition: "Ice Stone or near a Ice Rock",
+	},
+	gliscor: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Razor Fang",
+		evoCondition: "at night",
+	},
+	probopass: {
+		inherit: true,
+		evoType: "other",
+		evoCondition: "Thunder Stone or near a special magnetic field",
+	},
+	dusknoir: {
+		inherit: true,
+		evoType: "useItem",
+		evoItem: "Reaper Cloth",
+	},
     dialgaorigin: {
 		inherit: true,
 		requiredItem: undefined
@@ -78,5 +170,29 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	arceuswater: {
 		inherit: true,
 		requiredItems: undefined,
+	},
+	dewott: {
+		inherit: true,
+		evos: ["Samurott-Hisui"],
+	},
+	petilil: {
+		inherit: true,
+		evos: ["Lilligant-Hisui"],
+	},
+	rufflet: {
+		inherit: true,
+		evos: ["Braviary-Hisui"],
+	},
+	goomy: {
+		inherit: true,
+		evos: ["Sliggoo-Hisui"],
+	},
+	bergmite: {
+		inherit: true,
+		evos: ["Avalugg-Hisui"],
+	},
+	dartrix: {
+		inherit: true,
+		evos: ["Decidueye-Hisui"],
 	},
 };
