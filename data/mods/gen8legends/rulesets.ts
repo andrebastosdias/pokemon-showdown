0afa2d3775bd018894d17f5efdedb5001ddb25e4
André Bastos Dias <andrebastosdias@gmail.com>
2024-04-15 14:16:13 +0100

    Fix shiny Enamorus legality

--- data/mods/gen8legends/rulesets.ts
+++ data/mods/gen8legends/rulesets.ts
@@ -5,8 +5,23 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		onValidateSet(set) {
 			const species = this.dex.species.get(set.species);
 			const ability = this.dex.abilities.get(set.ability);
+			const item = this.dex.items.get(set.item);
+
+			if (item.id) {
+				set.item = '';
+			}
+
+			// Cherrim, Regigigas, Arceus
+			if (![421, 486, 493].includes(species.num)) {
+				set.ability = 'No Ability';
+			}
+
 			if (!Object.values(species.abilities).includes(ability.name)) {
 				return [`${set.species} can't have ${set.ability}.`];
+			}
+
+			if (species.baseSpecies === "Enamorus" && set.shiny) {
+				return [`${set.species} must not be shiny.`]
 			}
 		},
 	},
