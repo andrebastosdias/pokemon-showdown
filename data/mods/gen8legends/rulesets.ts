2e561d3204c6ca85e3862ab2175f6774423d65cd
André Bastos Dias <andrebastosdias@gmail.com>
2024-04-15 15:12:08 +0100

    Remove unnecessary checks

--- data/mods/gen8legends/rulesets.ts
+++ data/mods/gen8legends/rulesets.ts
@@ -5,16 +5,6 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		onValidateSet(set) {
 			const species = this.dex.species.get(set.species);
 			const ability = this.dex.abilities.get(set.ability);
-			const item = this.dex.items.get(set.item);
-
-			if (item.id) {
-				set.item = '';
-			}
-
-			// Cherrim, Regigigas, Arceus
-			if (![421, 486, 493].includes(species.num)) {
-				set.ability = 'No Ability';
-			}
 
 			if (!Object.values(species.abilities).includes(ability.name)) {
 				return [`${set.species} can't have ${set.ability}.`];
