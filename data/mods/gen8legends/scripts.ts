ada8357a8f1c299118732df28c922d3a7fb851e7
André Bastos Dias <andrebastosdias@gmail.com>
2024-06-17 15:44:45 +0100

    Arceus doesn't need Multitype

--- data/mods/gen8legends/scripts.ts
+++ data/mods/gen8legends/scripts.ts
@@ -4,8 +4,8 @@ export const Scripts: ModdedBattleScriptsData = {
 	init() {
 		this.modData('Abilities', 'noability').isNonstandard = null;
 		for (const i in this.data.Pokedex) {
-			// Cherrim, Regigigas, Arceus
-			if (![421, 486, 493].includes(this.modData('Pokedex', i).num)) {
+			// Cherrim, Regigigas
+			if (![421, 486].includes(this.modData('Pokedex', i).num)) {
 				this.modData('Pokedex', i).abilities = {0: 'No Ability'};
 			}
 			delete this.modData('Pokedex', i).requiredItem;
