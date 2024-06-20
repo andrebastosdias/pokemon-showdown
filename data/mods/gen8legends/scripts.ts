ef8ae1d4380e0670bfc49dd4df509ec99aefba7f
André Bastos Dias <andrebastosdias@gmail.com>
2024-06-21 00:29:58 +0100

    Fix Gigantamax and CAP illegality

--- data/mods/gen8legends/scripts.ts
+++ data/mods/gen8legends/scripts.ts
@@ -4,9 +4,6 @@ export const Scripts: ModdedBattleScriptsData = {
 	init() {
 		this.modData('Abilities', 'noability').isNonstandard = null;
 		for (const i in this.data.Pokedex) {
-			if (['Gigantamax', 'CAP'].includes(this.modData('Pokedex', i).isNonstandard)) {
-				this.modData('Pokedex', i).isNonstandard = 'Past';
-			}
 			// Cherrim, Regigigas
 			if (![421, 486].includes(this.modData('Pokedex', i).num)) {
 				this.modData('Pokedex', i).abilities = {0: 'No Ability'};
@@ -16,6 +13,11 @@ export const Scripts: ModdedBattleScriptsData = {
 		}
 		for (const i in this.data.Moves) {
 			this.modData('Moves', i).noPPBoosts = true;
+		}
+		for (const i in this.data.FormatsData) {
+			if (['Gigantamax', 'CAP'].includes(this.modData('FormatsData', i).isNonstandard)) {
+				this.modData('FormatsData', i).isNonstandard = 'Past';
+			}
 		}
 	},
 	side: {
