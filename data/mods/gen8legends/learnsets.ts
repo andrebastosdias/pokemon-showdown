f0c6d25a0251372529c58e443ff4d776e98c8d95
André Bastos Dias <andrebastosdias@gmail.com>
2024-07-21 17:04:57 +0100

    Refactor types for Lowercase<string>

--- data/mods/gen8legends/learnsets.ts
+++ data/mods/gen8legends/learnsets.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 
-export const Learnsets: {[k: string]: ModdedLearnsetData} = {
+export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTable = {
 	pikachu: {
 		learnset: {
 			babydolleyes: ["8T"],
