3ba953a849639fa01bd71cbd3157a46b0d2afa2c
André Bastos Dias <andrebastosdias@gmail.com>
2025-02-08 02:06:03 +0000

    Fix Arceus-Legend

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -2872,10 +2872,6 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		doublesTier: "DUber",
 		natDexTier: "Uber",
 	},
-	arceuslegend: {
-		isNonstandard: "Custom", // Only in Pokemon Legends: Arceus
-		tier: "Illegal",
-	},
 	victini: {
 		isNonstandard: "Past",
 		tier: "Illegal",
