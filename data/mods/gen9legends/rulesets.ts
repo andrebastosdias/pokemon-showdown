eed9a11a9c5dbcb9ce0019a55bcdc3d523a96afd
André Bastos Dias <andrebastosdias@gmail.com>
2025-11-08 18:31:37 +0000

    Legends Z-A: Add min team size

--- data/mods/gen9legends/rulesets.ts
+++ data/mods/gen9legends/rulesets.ts
@@ -4,7 +4,7 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 		effectType: 'ValidatorRule',
 		name: 'Flat Rules',
 		ruleset: [
-			'Obtainable', 'Species Clause', 'Nickname Clause', 'Item Clause = 1', 'Adjust Level = 50', 'Max Team Size = 3',
+			'Obtainable', 'Species Clause', 'Nickname Clause', 'Item Clause = 1', 'Adjust Level = 50', 'Min Team Size = 3', 'Max Team Size = 3',
 		],
 		banlist: ['Mythical', 'Restricted Legendary'],
 	},
