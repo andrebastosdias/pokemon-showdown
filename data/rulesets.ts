7bd9ebf5ca119809b9ef11baae91f945804156cc
André Bastos Dias <andrebastosdias@gmail.com>
2026-03-23 14:23:19 +0000

    Add Deoxys check in Hackmons

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -170,51 +170,6 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 		ruleset: ['Obtainable Moves', 'Obtainable Abilities', 'Obtainable Formes', 'EV Limit = Auto', 'Obtainable Misc'],
 		banlist: ['Unreleased', 'Unobtainable', 'Nonexistent'],
 		// Mostly hardcoded in team-validator.ts
-		onValidateTeam(team, format) {
-			let kyuremCount = 0;
-			let necrozmaDMCount = 0;
-			let necrozmaDWCount = 0;
-			let calyrexCount = 0;
-			for (const set of team) {
-				if (set.species === 'Kyurem-White' || set.species === 'Kyurem-Black') {
-					if (kyuremCount > 0) {
-						return [
-							`You cannot have more than one Kyurem-Black/Kyurem-White.`,
-							`(It's untradeable and you can only make one with the DNA Splicers.)`,
-						];
-					}
-					kyuremCount++;
-				}
-				if (set.species === 'Necrozma-Dusk-Mane') {
-					if (necrozmaDMCount > 0) {
-						return [
-							`You cannot have more than one Necrozma-Dusk-Mane`,
-							`(It's untradeable and you can only make one with the N-Solarizer.)`,
-						];
-					}
-					necrozmaDMCount++;
-				}
-				if (set.species === 'Necrozma-Dawn-Wings') {
-					if (necrozmaDWCount > 0) {
-						return [
-							`You cannot have more than one Necrozma-Dawn-Wings`,
-							`(It's untradeable and you can only make one with the N-Lunarizer.)`,
-						];
-					}
-					necrozmaDWCount++;
-				}
-				if (set.species === 'Calyrex-Ice' || set.species === 'Calyrex-Shadow') {
-					if (calyrexCount > 0) {
-						return [
-							`You cannot have more than one Calyrex-Ice/Calyrex-Shadow.`,
-							`(It's untradeable and you can only make one with the Reins of Unity.)`,
-						];
-					}
-					calyrexCount++;
-				}
-			}
-			return [];
-		},
 	},
 	obtainablemoves: {
 		effectType: 'ValidatorRule',
@@ -232,6 +187,12 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 		effectType: 'ValidatorRule',
 		name: 'Obtainable Formes',
 		desc: "Makes sure in-battle formes only appear in-battle.",
+		// Hardcoded in team-validator.ts
+	},
+	obtainablecartridgeformes: {
+		effectType: 'ValidatorRule',
+		name: 'Obtainable Cartridge Formes',
+		desc: "Validates groups of formes that are not obtainable together in the same cartridge.",
 		// Hardcoded in team-validator.ts
 	},
 	obtainablemisc: {
@@ -2764,6 +2725,7 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 		name: "Hackmons Forme Legality",
 		desc: `Enforces proper forme legality for hackmons-based metagames.`,
 		banlist: ['CAP', 'Future'],
+		// Different formes of Deoxys in Gen 3 hardcoded in team-validator.ts
 		onChangeSet(set, format, setHas, teamHas) {
 			let species = this.dex.species.get(set.species);
 			if (
