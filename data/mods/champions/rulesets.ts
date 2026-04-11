b2ea7d7a1906aa39bf10cfd5daffd07457993297
andrebastosdias <andrebastosdias@gmail.com>
2026-04-11 23:02:14 +0100

    Implement HP percentages and bar colors

--- data/mods/champions/rulesets.ts
+++ data/mods/champions/rulesets.ts
@@ -5,9 +5,6 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 			'Obtainable', 'Team Preview', 'Cancel Mod', 'Endless Battle Clause',
 			'Adjust Level = 50', 'Item Clause = 1',
 		],
-		onBegin() {
-			this.reportPercentages = true;
-		},
 	},
 	standard: {
 		inherit: true,
@@ -32,8 +29,5 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 		desc: "The in-game Flat Rules: Adjust Level 50, Species Clause, Item Clause = 1, -Mythical, -Restricted Legendary, Bring 6 Pick 3-6 depending on game type.",
 		ruleset: ['Obtainable', 'Team Preview', 'Species Clause', 'Nickname Clause', 'Item Clause = 1', 'Adjust Level = 50', 'Picked Team Size = Auto', 'Cancel Mod'],
 		banlist: ['Mythical', 'Restricted Legendary'],
-		onBegin() {
-			this.reportPercentages = true;
-		},
 	},
 };
