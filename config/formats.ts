2f556cc4bba0cc7b078f21c8b01ee8078c3ffe21
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2026-04-26 23:24:52 +0100

    ORAS Ubers: Ban Baton Pass
    https://www.smogon.com/forums/threads/baton-pass-in-oras-ubers.3779772/#post-10969031

--- config/formats.ts
+++ config/formats.ts
@@ -4841,7 +4841,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		mod: 'gen6',
 		searchShow: false,
 		ruleset: ['Standard', 'Mega Rayquaza Clause'],
-		banlist: ['Swagger'],
+		banlist: ['Baton Pass', 'Swagger'],
 	},
 	{
 		name: "[Gen 6] UU",
