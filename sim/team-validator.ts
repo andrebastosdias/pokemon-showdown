be0118e3ea4286afe77fe5af681ca8ee6c838029
andrebastosdias <andrebastosdias@gmail.com>
2026-05-03 22:14:46 +0100

    Add parenthesis

--- sim/team-validator.ts
+++ sim/team-validator.ts
@@ -1114,7 +1114,7 @@ export class TeamValidator {
 		if (!problems.length) {
 			if (!set.gender) {
 				if (this.gen <= 5 || ruleTable.has('obtainablemisc')) {
-					set.gender = species.gender || Math.random() <= species.genderRatio.M ? 'M' : 'F';
+					set.gender = species.gender || (Math.random() <= species.genderRatio.M ? 'M' : 'F');
 				} else {
 					set.gender = 'N';
 				}
